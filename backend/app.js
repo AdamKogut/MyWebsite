9473466c45bf37335360c46be98366d9534c3966
AdamKogut <akogut@purdue.edu>
2020-05-11 21:04:52 -0400

    Make app send the client correcly

--- backend/app.js
+++ backend/app.js
@@ -36,4 +36,13 @@ app.use(function(err, req, res, next) {
   res.render("error");
 });
 
+if (process.env.NODE_ENV == "production") {
+  // render the error page
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 module.exports = app;
