f9d83c7e7cbb1b95d0d3fd1a6b2e881d208ae2ea
AdamKogut <akogut@purdue.edu>
2022-09-14 00:31:50 -0400

    Remove warning, fix button outlining issue

--- backend/client/src/App.js
+++ backend/client/src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import Box from '@mui/material/Box';
-import Alert from '@mui/material/Alert';
-import Button from '@mui/material/Button';
-import Snackbar from '@mui/material/Snackbar';
 import DrawerMain from "./Menu/DrawerMain";
-import Resume from "./Images/AdamKogutResume.pdf";
 import InfoArea from "./InfoArea";
 import "./App.css";
 
@@ -15,13 +11,6 @@ export default function App() {
     <Box sx={{display:'flex'}}>
       <DrawerMain isOpen={isOpen} setIsOpen={setIsOpen}/>
       <InfoArea isOpen={isOpen} setIsOpen={setIsOpen}/>
-          <Snackbar open={true} anchorOrigin={{vertical:'bottom', horizontal:'right'}}>
-            <Alert severity="info" action={
-              <Button color="inherit" size="small" ><a href={Resume} download>DOWNLOAD</a></Button>
-            }>
-              This webpage is a work in progress and is being updated at this time. Sorry for the inconvience. Click download to download my latest Resume
-            </Alert>
-          </Snackbar>
     </Box>
   )
 }
