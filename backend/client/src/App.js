fbc73d88ad165b8669474444670fba498bac4dc5
AdamKogut <akogut@purdue.edu>
2022-09-14 00:13:30 -0400

    Add WIP warning to webpage

--- backend/client/src/App.js
+++ backend/client/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
 import DrawerMain from "./Menu/DrawerMain";
-import "./App.css";
+import Resume from "./Images/AdamKogutResume.pdf";
 import InfoArea from "./InfoArea";
+import "./App.css";
 
 export default function App() {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -11,6 +15,13 @@ export default function App() {
     <Box sx={{display:'flex'}}>
       <DrawerMain isOpen={isOpen} setIsOpen={setIsOpen}/>
       <InfoArea isOpen={isOpen} setIsOpen={setIsOpen}/>
+          <Snackbar open={true} anchorOrigin={{vertical:'bottom', horizontal:'right'}}>
+            <Alert severity="info" action={
+              <Button color="inherit" size="small" ><a href={Resume} download>DOWNLOAD</a></Button>
+            }>
+              This webpage is a work in progress and is being updated at this time. Sorry for the inconvience. Click download to download my latest Resume
+            </Alert>
+          </Snackbar>
     </Box>
   )
 }
